1919e003109c950027871bf4077cc0eda983029b
Wanny Martins <wcatery@gmail.com>
2022-01-25 16:00:54 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
-const data = require('../data/zoo_data');
+const data = require('../data/zoo_data')
+const animals = data.species;
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
+  return animals.filter((animal) => ids.includes(animal.id));
+
   // seu código aqui
 }
+console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae', '0938aa23-f153-4937-9f88-4858b24d6bce'));
+
 
 module.exports = getSpeciesByIds;
