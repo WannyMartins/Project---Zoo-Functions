2a81fd24fb391807a9c842bb9db39db2c41926db
Wanny Martins <wcatery@gmail.com>
2022-01-25 19:02:46 -0300

    requisito 4

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const trabalhadores = data.employees;
+
 function getEmployeeByName(employeeName) {
+  if (!employeeName) {
+    return {};
+  }
+  return trabalhadores.find((empregado) =>
+    employeeName.includes(empregado.firstName) || employeeName.includes(empregado.lastName));
   // seu código aqui
 }
-
+console.log(getEmployeeByName('Nigel'));
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
+const trabalhadores = data.employees;
+
 function isManager(id) {
+  return trabalhadores.some((empregado) =>
+    empregado.managers.includes(id));
   // seu código aqui
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  try {
+    if (isManager(managerId)) {
+      return trabalhadores
+        .filter((empregado) => empregado.managers.includes(managerId))
+        .map((empregados) =>
+          `${empregados.firstName} ${empregados.lastName}`);
+    }
+  } catch (error) {
+    throw error.message;
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
