35aa3649136ed31297e63a5174b4860bc0ca22d6
Wanny Martins <wcatery@gmail.com>
2022-01-25 16:09:02 -0300

    requisito 1 CORRIGINDO LINT

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,4 +1,5 @@
-const data = require('../data/zoo_data')
+const data = require('../data/zoo_data');
+
 const animals = data.species;
 
 function getSpeciesByIds(...ids) {
@@ -6,8 +7,5 @@ function getSpeciesByIds(...ids) {
 
   // seu código aqui
 }
-console.log(getSpeciesByIds());
-console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae', '0938aa23-f153-4937-9f88-4858b24d6bce'));
-
 
 module.exports = getSpeciesByIds;
