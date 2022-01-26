57daef32cb53bb24921955daaeff2c1bb6153243
Wanny Martins <wcatery@gmail.com>
2022-01-26 15:34:53 -0300

    requisito 5 pronto mas com erro de lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,19 +3,28 @@ const data = require('../data/zoo_data');
 const animals = data.species;
 
 function countAnimals(animal) {
-  const objeto = {};
   if (!animal) {
-    animals
-      .map((element) => {
-        objeto[element.name] = element.residents.length;
-        return objeto;
-      });
+    const objeto = {}
+    animals.map((element) => {
+      {
+        objeto[element.name] = element.residents.length,
+      }
+    });
+    return objeto;
   }
-
-  const ani = data
-    .map((bicho) => bicho.species);
-  return ani;
+  if (animal) {
+    if (animal.sex) {
+      const encontrarAnimal = animals.find((especie) => especie.name.includes(animal.specie));
+      const encontrarSexoAnimal = [encontrarAnimal].map((resident) => resident.residents
+        .map((sexo) => sexo.sex.includes(animal.sex)).filter((sex) => sex === true).length);
+      return `${encontrarSexoAnimal}`;
+    }
+    return animals.find((especie) => especie.name.includes(animal.specie)).residents.length;
+  }
 }
+console.log(countAnimals());
 
-console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
+console.log(countAnimals({ specie: 'bear' }));
+
 module.exports = countAnimals;
