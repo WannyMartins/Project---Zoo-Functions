6cfd6fee04f94c82ac387a1a01c7926d1c841a42
Wanny Martins <wcatery@gmail.com>
2022-02-07 10:47:46 -0300

    requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,14 +1,42 @@
 const data = require('../data/zoo_data');
 
-// const funcionarios = data.employees;
+// Tive ajuda de colegas Imar Mendes, Luá pra desenrrolar a logica,
 
-// const encontrarFuncionário = (parametro) {
-// }
+const { employees, species } = data;
+
+const encontrarFuncionário = ({ name, id }) => {
+  const funcionario = employees.find((employee) => employee.firstName === name
+    || employee.lastName === name
+    || employee.id === id);
+  return funcionario;
+};
+
+const responsavelPor = (parametro) => {
+  const mapFunc = encontrarFuncionário(parametro).responsibleFor;
+  return mapFunc.map((animal) => species.find((specie) => specie.id === animal).name);
+};
+
+const regioes = (parametro) => {
+  const mapLocal = encontrarFuncionário(parametro).responsibleFor;
+  return mapLocal.map((animal) => species.find((specie) => specie.id === animal).location);
+};
+
+const objetoRequerido = (parametro) => {
+  const primeiroFuncionario = encontrarFuncionário(parametro);
+  if (!primeiroFuncionario) throw new Error('Informações inválidas');
+  return {
+    id: primeiroFuncionario.id,
+    fullName: `${primeiroFuncionario.firstName} ${primeiroFuncionario.lastName}`,
+    species: responsavelPor(parametro),
+    locations: regioes(parametro),
+  };
+};
 
 function getEmployeesCoverage(parametro) {
-
-  //   // seu código aqui
+  if (!parametro) {
+    return employees.map((func) => objetoRequerido(func));
+  }
+  return objetoRequerido(parametro);
 }
-// console.log(getEmployeesCoverage('Bethea'));
 
 module.exports = getEmployeesCoverage;
